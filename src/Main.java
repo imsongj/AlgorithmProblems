f3c5a5fa6f714394c8f169c91c8f4123d127312d
imsongj <imsongj@gmail.com>
2023-02-25 14:19:41 +0900

    0225

--- src/Main.java
+++ src/Main.java
@@ -1,63 +1,85 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 //15683 감시
 //모든 경우 탐색, 조합(각 cctv 마다 4방향 경우를 체크해야한다; 5번일때는 불필요), 사다리랑 비슷하다: 각 cctv 마다, 각 방향마다 조합
 // 전체 빈 칸 - cctv감시 칸  = 사각지대
 //void findVisable(int cctv type, int r, int c, int direction) 1: 
 public class Main {
-	static final char[] VOWELS = {'a', 'e', 'i', 'o', 'u'};
-	static int L;
-	static int C;
-	static char[] letters;
-	static char[] password;
-	static StringBuilder sb;
+	static final int EMPTY = 0;
+	static final int WALL = 6;
+	static final int CCTV_SINGLE = 1;
+	static final int CCTV_DOUBLE_180 = 2;
+	static final int CCTV_DOUBLE_90 = 3;
+	static final int CCTV_TRIPLE = 4;
+	static final int CCTV_FULL = 5;
+	static final int[] CCTV_POSSIBLE_DIRECTIONS = {0, 4, 2, 4, 4, 1}; //cctv 별로 탐색해야하는 방향 수
 	
+	static int[] dr = {0, 1, 0, -1}; //right, down, left, up
+	static int[] dc = {1, 0, -1, 0};
+	
+	static int N;
+	static int M;
+	static int[][] office;
+	
+	static int cctvCount;
+	static List<CCTV> cctvs;
+	static int[] directions;
+	
+	static int count1 = 0;
+	static class CCTV {
+		int type;
+		int possibleDirections;
+		int r;
+		int c;
+		public CCTV(int type, int r, int c) {
+			this.type = type;
+			this.possibleDirections = CCTV_POSSIBLE_DIRECTIONS[type];
+			this.r = r;
+			this.c = c;
+		}
+	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		sb = new StringBuilder();
 		String[] input = br.readLine().split(" ");
-		L = Integer.parseInt(input[0]); 
-		C = Integer.parseInt(input[1]); 
+		N = Integer.parseInt(input[0]);
+		M = Integer.parseInt(input[1]);
 		
-		letters = new char[C];
-		password = new char[L];
-		input = br.readLine().trim().split(" ");
-		for (int i = 0; i < C; i++) {
-			letters[i] = input[i].charAt(0);
+		office = new int[N][M];
+		cctvCount = 0;
+		cctvs = new ArrayList<>(8);
+		for (int r = 0; r < N; r++) {
+			input = br.readLine().split(" ");
+			for (int c = 0; c < M; c++) {
+				office[r][c] = Integer.parseInt(input[c]);
+				if (office[r][c] > 0 && office[r][c] < 6) {
+					int type = office[r][c];
+					cctvCount++;
+					cctvs.add(new CCTV(type, r, c));
+				}
+			}
 		}
-		Arrays.sort(letters);
+		directions = new int[cctvCount];
 		getCombination(0, 0);
-		System.out.println(sb.toString());
+		System.out.println(count1);
 	}
 	
-	public static void getCombination(int count, int start) {
-		if (count == L) {
-			if (checkPassword()) {
-				for (int i = 0; i < L; i++) {
-					sb.append(password[i]);
-				}
-				sb.append('\n');
-			}
+	public static void getCombination(int count, int start) { //cctv마다 방향 설정
+		if (count == cctvCount) {
+			//사각지대 카운트
+			//System.out.println(Arrays.toString(directions));
+			count1++;
 			return;
 		}
-		for (int i = start; i < C; i++) {
-			password[count] = letters[i];
-			getCombination(count + 1, i + 1);
-		}
-	}
-	
-	public static boolean checkPassword() {
-		int vowelCount = 0;
-		for (int i = 0; i < L; i++) {
-			for (int j = 0; j < VOWELS.length; j++) {
-				if (password[i] == VOWELS[j]) {
-					vowelCount++;
-				}
+		for (int cctvNum = start; cctvNum < cctvCount; cctvNum++) {
+			for (int d = 0; d < cctvs.get(cctvNum).possibleDirections; d++) {
+				directions[cctvNum] = d;
+				getCombination(count + 1, cctvNum + 1);
 			}
 		}
-		return vowelCount >= 1 && L - vowelCount >= 2;
 	}
 }
