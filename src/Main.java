efc18fe33ff2f824d7ab1996f7dfc4b89183d9bc
imsongj <imsongj@gmail.com>
2023-02-26 22:29:25 +0900

    0226

--- src/Main.java
+++ src/Main.java
@@ -17,8 +17,9 @@ public class Main {
 	static final int CCTV_DOUBLE_90 = 3;
 	static final int CCTV_TRIPLE = 4;
 	static final int CCTV_FULL = 5;
+	static final int[] CCTV_NUMBER_OF_CAMERA = {0, 1, 2, 2, 3, 4};
 	static final int[] CCTV_POSSIBLE_DIRECTIONS = {0, 4, 2, 4, 4, 1}; //cctv 별로 탐색해야하는 방향 수
-	
+
 	static int[] dr = {0, 1, 0, -1}; //right, down, left, up
 	static int[] dc = {1, 0, -1, 0};
 	
@@ -26,11 +27,12 @@ public class Main {
 	static int M;
 	static int[][] office;
 	
+	static int blindCount = 0;
 	static int cctvCount;
 	static List<CCTV> cctvs;
 	static int[] directions;
-	
-	static int count1 = 0;
+	static boolean[][] visited;
+	static int minCount = Integer.MAX_VALUE;
 	static class CCTV {
 		int type;
 		int possibleDirections;
@@ -42,13 +44,64 @@ public class Main {
 			this.r = r;
 			this.c = c;
 		}
+		
+		public int getCount(int direction) {
+			int count = 0;
+			for (int i = 0; i < CCTV_NUMBER_OF_CAMERA[type]; i++) {
+				if (type == CCTV_SINGLE) {
+					count += view(direction);
+				}
+				if (type == CCTV_DOUBLE_180) { //0(0, 2), 1(1,3)
+					int d = direction + i * 2; 
+					count += view(d);
+				}
+				if (type == CCTV_DOUBLE_90) {
+					int d =(direction + i) % 4; 
+					count += view(d);
+				}
+				if (type == CCTV_TRIPLE) {
+					int d =(direction + i) % 4; 
+					count += view(d);
+				}
+				if (type == CCTV_FULL) {
+					int d =(direction + i) % 4; 
+					count += view(d);
+				}
+			}
+			return count;
+		}
+		
+		private int view(int direction) {
+			int count = 0;
+			int newR = r;
+			int newC = c;;
+			while (true) { //주어진 방향으로 일직선 최대 칸 카운트
+				newR = newR + dr[direction];
+				newC = newC + dc[direction];
+				
+				if (newR < 0 || newR >= N || newC < 0 || newC >= M) {
+					break;
+				}
+				if (office[newR][newC] == WALL) {
+					break;
+				}
+				if (office[newR][newC] > 0 || visited[newR][newC]) { //다른 cctv, 이미 다른 cctv가 방문한 칸인 경우 카운트 하지 않는다
+					continue;
+				}
+				count++;
+				visited[newR][newC] = true;
+			}
+			return count;
+		}
 	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] input = br.readLine().split(" ");
 		N = Integer.parseInt(input[0]);
 		M = Integer.parseInt(input[1]);
 		
+		blindCount = N * M;
 		office = new int[N][M];
 		cctvCount = 0;
 		cctvs = new ArrayList<>(8);
@@ -56,23 +109,27 @@ public class Main {
 			input = br.readLine().split(" ");
 			for (int c = 0; c < M; c++) {
 				office[r][c] = Integer.parseInt(input[c]);
-				if (office[r][c] > 0 && office[r][c] < 6) {
+				if (office[r][c] == 6) {
+					blindCount--;
+					continue;
+				}
+				if (office[r][c] > 0) { //cctv 정보 저장
 					int type = office[r][c];
 					cctvCount++;
 					cctvs.add(new CCTV(type, r, c));
+					blindCount--;
 				}
 			}
 		}
 		directions = new int[cctvCount];
 		getCombination(0, 0);
-		System.out.println(count1);
+		System.out.println(minCount);
 	}
 	
 	public static void getCombination(int count, int start) { //cctv마다 방향 설정
 		if (count == cctvCount) {
 			//사각지대 카운트
-			//System.out.println(Arrays.toString(directions));
-			count1++;
+			minCount = Math.min(minCount, blindCount - countCovered());
 			return;
 		}
 		for (int cctvNum = start; cctvNum < cctvCount; cctvNum++) {
@@ -81,5 +138,15 @@ public class Main {
 				getCombination(count + 1, cctvNum + 1);
 			}
 		}
+	}
+	
+	public static int countCovered() {
+		//cctv마다 주어진 방향과 cctv 타입별로 보드 탐색; visited 배열 필요
+		visited = new boolean[N][M];
+		int totalCoveredCount = 0;
+		for (int i = 0; i < cctvCount; i++) {
+			totalCoveredCount += cctvs.get(i).getCount(directions[i]); 
+		}
+		return totalCoveredCount;
 	}
 }
