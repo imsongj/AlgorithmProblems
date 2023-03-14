df725534e0d3bae67bb08d5f8a855c65363e13dc
username <user@email.mail>
2023-03-14 17:53:58 +0900

    0314

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 //16398 행성연결
-//간선 수가 적기 때문에 kruskal 알고리즘 사용
+//간선 수가 많기 때문에 prim's 사용
 //1. 간선들 오름차순 정렬
 //2. union find 사용으로 사이클 방지
 // 	간선의 양끝 정점이 같은 집합에 속해 있는지를 먼저 검사
@@ -26,20 +28,45 @@ public class Main {
 		}
 		
 	}
-	static int[] parents;
+	static boolean[] visited;
+	static int N;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		parents = new int[N];
-		String[] line;
-		for (int i = 0; i < N; i++) {
-			line = br.readLine().split(" ");
-			int from = Integer.parseInt(line[0]);
-			int to = Integer.parseInt(line[1]);
-			int weight = Integer.parseInt(line[2]);
+		N = Integer.parseInt(br.readLine());
+		ArrayList<Edge>[] adj = new ArrayList[N + 1];
+		for (int i = 1; i <= N; i++) {
+			adj[i] = new ArrayList<>();
 		}
+		String[] line;
+		for (int r = 1; r <= N; r++) {
+			line = br.readLine().split(" ");
+			for (int c = 1; c <= N; c++) {
+				if (r == c) {
+					continue;
+				}
+				adj[r].add(new Edge(r, c, Integer.parseInt(line[c - 1])));
+			}
+		}
+		visited = new boolean[N + 1];
+		PriorityQueue<Edge> queue = new PriorityQueue<>();
+		int count = 1;
+		long result = 0;
+
+		queue.addAll(adj[1]); 
+		visited[1] = true;
 		
-		
+		while (count != N) {
+			Edge min = queue.poll();
+			if (visited[min.to]) {
+				continue;
+			}
+			
+			result += min.weight;
+			queue.addAll(adj[min.to]);
+			visited[min.to] = true;
+			count++;
+		}
+		System.out.println(result);
 	}
-	
 }
