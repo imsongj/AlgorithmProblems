5b63f8b8f7d8b57128997ed0e12a6918a594a459
imsongj <imsongj@gmail.com>
2023-09-13 14:45:29 +0900

    0913

--- src/Main_1003_피보나치함수.java
+++ src/Main_1003_피보나치함수.java
@@ -1,14 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.PriorityQueue;
 
-public class Main {
+public class Main_1003_피보나치함수 {
     /*
     1003 피보나치함수
 
